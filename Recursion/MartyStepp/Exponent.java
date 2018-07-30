3b68f1ff7441ae31db36920c7af36c997a7c23df
Archana <archana12n@gmail.com>
2018-07-30 14:12:18 -0700

    Exponent

--- Recursion/MartyStepp/Exponent.java
+++ Recursion/MartyStepp/Exponent.java
@@ -1,5 +1,38 @@
 package Recursion.MartyStepp;
 
 public class Exponent {
+    public static void main(String args[]){
+        System.out.print(exponent(2,5));
+        System.out.println(exponentOptimiseed(2,4));
+    }
+    /* 2^5         32
+          2*(2^4)    2*16
+             2*(2^3)    2*8
+                2*(2^2)    2*4
+                   2*(2^1)    2*2
+                       2*(2^0)   2*1
 
+    * */
+     static int exponent(int base, int expo){
+        if(expo == 0) return 1;
+        return base*exponent(base,expo-1);
+    }
+
+    /*
+      EVEN             ODD
+     2*2*2*2         2*2*2
+        2^2*2^2
+
+        2^4
+
+
+    * */
+    static int exponentOptimiseed(int base,int expo){
+        if(expo == 1) return base;
+        int value = exponentOptimiseed(base,expo/2);
+        if(expo % 2 == 0){
+          return value*value;
+        }
+        return value*value*base;
+    }
 }
